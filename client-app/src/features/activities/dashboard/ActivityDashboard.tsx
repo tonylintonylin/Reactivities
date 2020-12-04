15642fb500722bfc3565611863506e91111c75ea
Tony Lin <tonylinbusiness@gmail.com>
2020-12-04 11:36:37 -0500

    Added edit mode to display form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,12 +9,16 @@ interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
   selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
 }
 
 export const ActivityDashboard: React.FC<IProps> = ({
   activities,
   selectActivity,
   selectedActivity,
+  editMode,
+  setEditMode,
 }) => {
   return (
     <Grid>
@@ -25,8 +29,13 @@ export const ActivityDashboard: React.FC<IProps> = ({
         ></ActivityList>
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
-        <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+          />
+        )}
+        {editMode && <ActivityForm />}
       </Grid.Column>
     </Grid>
   );
