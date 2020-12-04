0c92041a2e86855736ba81b031464cfcf94e5b26
Tony Lin <tonylinbusiness@gmail.com>
2020-12-04 10:17:29 -0500

    Organized folder structure

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
-import "./App.css";
 import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
 
