9919828897a4b09d7762df01c74bf679c347a698
Tony Lin <tonylinbusiness@gmail.com>
2020-12-04 13:29:24 -0500

    Added loading indicator for writing data

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,8 +15,8 @@ const App = () => {
   );
 
   const [editMode, setEditMode] = useState(false);
-
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter((a) => a.id === id)[0]);
@@ -29,27 +29,36 @@ const App = () => {
   };
 
   const handleCreateActivity = (activity: IActivity) => {
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    });
+    setSubmitting(true);
+    agent.Activities.create(activity)
+      .then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
   const handleEditActivity = (activity: IActivity) => {
-    agent.Activities.update(activity).then(() => {
-      setActivities([
-        ...activities.filter((a) => a.id !== activity.id, activity),
-      ]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    });
+    setSubmitting(true);
+    agent.Activities.update(activity)
+      .then(() => {
+        setActivities([
+          ...activities.filter((a) => a.id !== activity.id, activity),
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
 
   const handleDeleteActivity = (id: string) => {
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((a) => a.id !== id)]);
-    });
+    setSubmitting(true);
+    agent.Activities.delete(id)
+      .then(() => {
+        setActivities([...activities.filter((a) => a.id !== id)]);
+      })
+      .then(() => setSubmitting(false));
   };
 
   useEffect(() => {
@@ -81,6 +90,7 @@ const App = () => {
           createActivity={handleCreateActivity}
           editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </>
