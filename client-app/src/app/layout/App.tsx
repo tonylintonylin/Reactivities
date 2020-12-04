cde9d4a6f091dd6314904d8c719dd4e00f4e60d8
Tony Lin <tonylinbusiness@gmail.com>
2020-12-04 10:42:27 -0500

    Added navbar and assets

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Header, Icon, List } from "semantic-ui-react";
-import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
+import React, { useState, useEffect } from "react";
+import { List } from "semantic-ui-react";
+import axios from "axios";
 import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -17,10 +18,7 @@ const App = () => {
 
   return (
     <div>
-      <Header as="h2">
-        <Icon name="users" />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
+      <NavBar />
       <List>
         {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
