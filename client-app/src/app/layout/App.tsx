e2f68110d05408bdf94a6d2fdfce2ba69182299f
Tony Lin <tonylinbusiness@gmail.com>
2020-12-04 10:19:15 -0500

    Getting list of activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,13 +5,13 @@ import "semantic-ui-css/semantic.min.css";
 
 class App extends Component {
   state = {
-    values: [],
+    activities: [],
   };
 
   componentDidMount() {
-    axios.get("http://localhost:5000/api/values").then((response) => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
       this.setState({
-        values: response.data,
+        activities: response.data,
       });
     });
   }
@@ -24,8 +24,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activity: any) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </>
